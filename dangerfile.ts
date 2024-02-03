cbf4442807c369601f1aee672f78df185c1b218b
Rozamo <35562703+Rozamo@users.noreply.github.com>
2024-02-03 19:26:58 +0530

    refactor: changelog to changeset

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,12 +4,12 @@ const hasChangelog = danger.git.created_files.find((fileName) =>
   new RegExp("^.changeset/[a-z]+[a-z,-]*[a-z]+.md$").test(fileName),
 );
 
-const hasSkipChangelogLabel = danger.github.issue.labels.find(
-  (label) => label.name === "changelog not required",
+const hasSkipChangesetLabel = danger.github.issue.labels.find(
+  (label) => label.name === "changeset not required",
 );
 
-if (!hasChangelog && !hasSkipChangelogLabel) {
+if (!hasChangelog && !hasSkipChangesetLabel) {
   fail(
-    "Please add a changelog entry for your changes.\n Check: https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md#i-am-in-a-single-package-repository for info on how to add changeset",
+    "Please add a changeset entry for your changes.\n Check: https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md#i-am-in-a-single-package-repository for info on how to add changeset",
   );
 }
