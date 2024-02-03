2f4808873e32d46ec5088e91ccd40acd309f822d
Rozamo <35562703+Rozamo@users.noreply.github.com>
2024-02-03 19:24:05 +0530

    feat: check if label skips changeset in danger js

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,6 +4,12 @@ const hasChangelog = danger.git.created_files.find((fileName) =>
   new RegExp("^.changeset/[a-z]+[a-z,-]*[a-z]+.md$").test(fileName),
 );
 
-if (!hasChangelog) {
-  fail("Please add a changelog entry for your changes.");
+const hasSkipChangelogLabel = danger.github.issue.labels.find(
+  (label) => label.name === "changelog not required",
+);
+
+if (!hasChangelog && !hasSkipChangelogLabel) {
+  fail(
+    "Please add a changelog entry for your changes.\n Check: https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md#i-am-in-a-single-package-repository for info on how to add changeset",
+  );
 }
